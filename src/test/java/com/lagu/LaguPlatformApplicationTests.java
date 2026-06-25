be0a4adfa739eed9b56efe4e925486c5d3a30d7c
santoshambali <49180568+santoshambali@users.noreply.github.com>
2026-06-25 17:57:00 +0530

    first commit

---
+++ src/test/java/com/lagu/LaguPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lagu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaguPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
